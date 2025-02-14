9177cd809b68f97303c19be65598ee0e12e77387
AngelaBruh <angelabruna836@gmail.com>
2025-02-14 15:28:35 -0300

    tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se o cep começa com zero utilize outro tipo
+        long cpf = 98765432109L; // se o cpf começa com zero, utilize outro tipo
+        float pi = 3.14F; // o tipo float necessita do F ou f no final, é a mesma coisa para o tipo long
+
     }
 }
