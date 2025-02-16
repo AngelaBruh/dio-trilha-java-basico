3d4cd6cfd47912c1d571edf9a92728976d67f2fa
AngelaBruh <angelabruna836@gmail.com>
2025-02-16 17:00:43 -0300

    calculadora

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,5 +1,23 @@
+// Aqui uma exemplificação de documentação do codigo:
+
+/*
+ * @author Bruna Angela
+ * @version 1.0
+ * @version 01/01/2025
+ */
+
+
 public class Calculadora {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
     }
+
+
+    /*
+     Utilizando este comando no terminal para criação da documentação do codigo:
+     javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+    
+    
+     */
 }
