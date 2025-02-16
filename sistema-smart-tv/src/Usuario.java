e9440a337fd2807080716f3f37e5f22d03a257f9
AngelaBruh <angelabruna836@gmail.com>
2025-02-16 14:38:20 -0300

    smartTv

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -3,9 +3,31 @@ public class Usuario {
         
         SmartTv smarTv = new SmartTv();
 
-        System.out.println("TV Ligada? " + smarTv.ligada);
+        smarTv.diminuirVolume();
+        smarTv.diminuirVolume();
+        smarTv.diminuirVolume();
+        smarTv.aumentarVolume();
+
         System.out.println("Canal Atual? " + smarTv.canal);
+
+        smarTv.mudarCanal(13);
+
+        System.out.println("Canal Atual? " + smarTv.canal);
+
+        System.out.println("Volume Atual: " + smarTv.volume);
+
+        System.out.println("TV Ligada? " + smarTv.ligada);
         System.out.println("Volume Atual? " + smarTv.volume);
+
+
+        smarTv.ligar();
+        System.out.println("Novo Status -> TV Ligada? " + smarTv.ligada);
         
+        smarTv.desligar();
+        System.out.println("Novo Status -> TV Ligada? " + smarTv.ligada);
+
+
+
+
     }
 }
