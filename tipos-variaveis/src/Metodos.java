0f17d94750b686d5b321c32722a9cfe4a627db07
AngelaBruh <angelabruna836@gmail.com>
2025-02-16 14:08:44 -0300

    metodos

--- tipos-variaveis/src/Metodos.java
+++ tipos-variaveis/src/Metodos.java
@@ -2,5 +2,7 @@ public class Metodos {
     // Metódos são funções, ações ou subrotinas
     // Deve ser nomeado como verbo por exemplo correr, acelerar, falar
     // Todas as letras do nome são minusculas com exceção da primeira letra da segunda palavra
-    
+    // Metódos são definidos dentro de uma classe
+    // Deve ser analisado se o metódo retornará ou não algum valor
+    // Caso não retorne nenhum valor, será representado pela palavra-chave void
 }
