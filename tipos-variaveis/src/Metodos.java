1842f493f58bbc471d7406e3f16f5a125d425fb0
AngelaBruh <angelabruna836@gmail.com>
2025-02-16 13:53:22 -0300

    metodos

--- tipos-variaveis/src/Metodos.java
+++ tipos-variaveis/src/Metodos.java
@@ -1,3 +1,6 @@
 public class Metodos {
+    // Metódos são funções, ações ou subrotinas
+    // Deve ser nomeado como verbo por exemplo correr, acelerar, falar
+    // Todas as letras do nome são minusculas com exceção da primeira letra da segunda palavra
     
 }
