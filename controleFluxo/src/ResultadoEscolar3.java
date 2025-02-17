39528d36c4edf7179230748ca9b2fc0d80a82244
AngelaBruh <angelabruna836@gmail.com>
2025-02-17 13:54:31 -0300

    resultado escolar

---
+++ controleFluxo/src/ResultadoEscolar3.java
@@ -0,0 +1,9 @@
+public class ResultadoEscolar3 {
+    public static void main(String[] args) {
+		int nota = 7;
+
+        // Condição ternária - para se ter um codigo limpo
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+	}
+}
